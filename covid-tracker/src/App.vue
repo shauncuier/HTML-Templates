57db91ba08bb2b0d54468724649df655c50d1444
Shaun <shauncuier@gmail.com>
2021-07-20 03:17:08 +0600

    tailwind

--- covid-tracker/src/App.vue
+++ covid-tracker/src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header />
+
   <router-view/>
+  </div>
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+import Header from '@/components/Header'
+export default {
+  components: {
+    Header
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ covid-tracker/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+// module.exports = {
+//     plugins: [
+//       require('@tailwindcss/forms'),
+//       require('@tailwindcss/aspect-ratio'),
+//       require('@tailwindcss/typography'),
+//       require('tailwindcss-children'),
+//     ],
+//   }
